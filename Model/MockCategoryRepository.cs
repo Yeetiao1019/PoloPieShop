4a706d23e67365b48f2e2ba1991b99ee7b2e4202
YeeTiao1019 <n1533512@gmail.com>
2021-12-29 17:32:01 +0800

    新增 Pie, Category 的介面與模擬資料，並註冊到 DI

---
+++ Model/MockCategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoloPieShop.Model
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => new List<Category>
+        {
+            new Category{ CategoryId = 1, CategoryName= "甜派",Description="甜甜的"},
+            new Category{ CategoryId = 2, CategoryName= "鹹派",Description="鹹鹹的"},
+            new Category{ CategoryId = 1, CategoryName= "減糖派",Description="不太甜的"}
+        };
+    }
+}
