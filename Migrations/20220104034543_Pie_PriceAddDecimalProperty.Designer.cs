115fb0714a319b8d42532f048f3c42cb487a7d46
YeeTiao1019 <n1533512@gmail.com>
2022-01-04 11:50:34 +0800

    將 Pie.Price 欄位格式設定為 decimal(18,4)

--- Migrations/20220104034543_Pie_PriceAddDecimalProperty.Designer.cs
+++ Migrations/20220104034543_Pie_PriceAddDecimalProperty.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PoloPieShop.Models;
 
 namespace PoloPieShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220104034543_Pie_PriceAddDecimalProperty")]
+    partial class Pie_PriceAddDecimalProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
