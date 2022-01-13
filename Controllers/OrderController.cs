efb9c2aeb085a5566bac23158d981905ae3887dd
YeeTiao1019 <n1533512@gmail.com>
2022-01-13 13:41:34 +0800

    加入結帳邏輯與結帳完成頁面

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -20,7 +20,36 @@ namespace PoloPieShop.Controllers
 
         public IActionResult Checkout()
         {
-            return View("Checkout");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var Items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = Items;
+
+            if(_shoppingCart.ShoppingCartItems.Count <= 0)
+            {
+                ModelState.AddModelError("", "您的購物車內沒有商品。");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+
+                return RedirectToAction("CheckoutComplete");
+            }
+
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewData["CheckoutCompleteMsg"] = "謝謝您的支持，我們會以最快的時間處理您的訂單！";
+
+            return View();
         }
     }
 }
--- Models/Order.cs
+++ Models/Order.cs
@@ -14,50 +14,50 @@ namespace PoloPieShop.Models
 
         public List<OrderDetail> OrderDetails { get; set; }
 
-        [Required(ErrorMessage = "Please enter your first name")]
-        [Display(Name = "First name")]
+        [Required(ErrorMessage = "請輸入名子")]
+        [Display(Name = "名子")]
         [StringLength(50)]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your last name")]
-        [Display(Name = "Last name")]
+        [Required(ErrorMessage = "請輸入姓氏")]
+        [Display(Name = "姓氏")]
         [StringLength(50)]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Please enter your address")]
+        [Required(ErrorMessage = "請輸入聯絡地址")]
         [StringLength(100)]
-        [Display(Name = "Address Line 1")]
+        [Display(Name = "聯絡地址 1")]
         public string AddressLine1 { get; set; }
 
-        [Display(Name = "Address Line 2")]
+        [Display(Name = "聯絡地址 2")]
         public string AddressLine2 { get; set; }
 
-        [Required(ErrorMessage = "Please enter your zip code")]
-        [Display(Name = "Zip code")]
-        [StringLength(10, MinimumLength = 4)]
+        [Required(ErrorMessage = "請輸入郵遞區號")]
+        [Display(Name = "郵遞區號")]
+        [StringLength(10, MinimumLength = 3)]
         public string ZipCode { get; set; }
 
-        [Required(ErrorMessage = "Please enter your city")]
+        [Required(ErrorMessage = "請輸入縣市")]
         [StringLength(50)]
         public string City { get; set; }
         [StringLength(10)]
         public string State { get; set; }
 
-        [Required(ErrorMessage = "Please enter your country")]
+        [Required(ErrorMessage = "請輸入國家")]
         [StringLength(50)]
         public string Country { get; set; }
 
-        [Required(ErrorMessage = "Please enter your phone number")]
+        [Required(ErrorMessage = "請輸入聯絡電話")]
         [StringLength(25)]
         [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Phone number")]
+        [Display(Name = "聯絡電話")]
         public string PhoneNumber { get; set; }
 
         [Required]
         [StringLength(50)]
         [DataType(DataType.EmailAddress)]
         [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
-            ErrorMessage = "The email address is not entered in a correct format")]
+            ErrorMessage = "email 格式不正確")]
         public string Email { get; set; }
 
         [BindNever]
